597dca7d2337a7d2ce04b8731a5e0e7dbc7bbcb0
Marc Nygaard <marcnygaard100@gmail.com>
2023-11-07 18:11:23 +0100

    add cJSON to cMakeLists.txt

---
+++ deserializer/deserializer.c
@@ -0,0 +1,49 @@
+// product_parser.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "cJSON.h"
+#include "deserializer.h"
+
+Product* parse_products(const char *json_string) {
+    const cJSON *json_array = cJSON_Parse(json_string);
+    if (json_array == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            fprintf(stderr, "Error in JSON before: %s\n", error_ptr);
+        }
+        return NULL;
+    }
+
+    int array_size = cJSON_GetArraySize(json_array);
+    int *product_count = array_size;
+    Product *products = malloc(array_size * sizeof(Product));
+
+    if (products == NULL) {
+        fprintf(stderr, "Error: Unable to allocate memory for products\n");
+        cJSON_Delete(json_array);
+        return NULL;
+    }
+
+    for (int i = 0; i < array_size; i++) {
+        cJSON *json_item = cJSON_GetArrayItem(json_array, i);
+        cJSON *temp;
+
+        temp = cJSON_GetObjectItemCaseSensitive(json_item, "expire_date");
+        products[i].expire_date = temp ? strdup(temp->valuestring) : NULL;
+
+        temp = cJSON_GetObjectItemCaseSensitive(json_item, "id");
+        products[i].id = temp ? strdup(temp->valuestring) : NULL;
+
+        temp = cJSON_GetObjectItemCaseSensitive(json_item, "title");
+        products[i].title = temp ? strdup(temp->valuestring) : NULL;
+
+        temp = cJSON_GetObjectItemCaseSensitive(json_item, "qty");
+        products[i].qty = temp ? temp->valueint : 0;
+
+        temp = cJSON_GetObjectItemCaseSensitive(json_item, "price");
+        products[i].price = temp ? temp->valueint : 0;
+    }
+
+    cJSON_Delete(json_array);
+    return products;
+}
